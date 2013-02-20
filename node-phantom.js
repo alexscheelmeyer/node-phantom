2038394a4f00b861fa7d4ff6c27adf182b0b5890
MrOrz <johnsonliang7@gmail.com>
2013-02-20 19:23:43 +0800

    Handling exit event of phantom child process in order to stop server after Phantom crashes.

--- node-phantom.js
+++ node-phantom.js
@@ -196,6 +196,17 @@ module.exports={
 			
 				callback(null,proxy);
 			});
+
+			// An exit event listener that is registered AFTER the phantomjs process
+			// is successfully created.
+			phantom.on('exit', function(code, signal){
+
+				// Close server upon phantom crash.
+				if(code !== 0 && signal === null){
+					console.warn('phantom crash: code '+code);
+					server.close();
+				}
+			});
 		});
 	}
 };
